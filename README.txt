1b9d809eddc40cf77275a50f4e6a27c7a1f32550
Dégi János <johann.degi@gmail.com>
2018-08-23 15:22:02 +0200

    Init commit

---
+++ README.txt
@@ -0,0 +1,11 @@
+GitHub
+
+tutorial
+https://www.youtube.com/watch?v=E8TXME3bzNs&t=1466s
+
+intellij tutorial
+https://www.youtube.com/watch?v=uUzRMOCBorg&t=38s
+
+https://www.jetbrains.com/help/idea/set-up-a-git-repository.html
+
+
---
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -0,0 +1,17 @@
+package guru.springframework.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webappApplication {
+
+	public static void main(String[] args) {
+
+		//Terminal: D:\workspace_spring\spring5webapp>mvnw spring-boot:run
+
+		//h2 (embedded) database console url: http://localhost:8080/h2-console
+
+		SpringApplication.run(Spring5webappApplication.class, args);
+	}
+}
